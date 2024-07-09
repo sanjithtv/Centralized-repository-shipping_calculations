92115b9c1793b1f033ea2f2925b11a194c065fd7
Sanjith TV <sanjith.tv@gmail.com>
2024-07-09 11:31:08 -0400

    modified py for revert

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by sanjithtv and updated in main for pull
+# Here is another update by sanjithtv
